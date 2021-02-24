847c8eeaf2d2de70d4afd20ffa30c0fc9d893e0f
Jacob Weinstock <jakobweinstock@gmail.com>
2021-02-23 23:55:41 -0700

    use binary not service call
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 cp -a /tink-reboot /host/usr/sbin/tink-reboot
 chroot /host <<EOT
 /usr/sbin/tink-reboot -install
-service tink-reboot stop
-service tink-reboot start
+/usr/sbin/tink-reboot -start
+/usr/sbin/tink-reboot -run
 EOT
