9343d8cb630c5d23800cae434693056503a8463e
Jacob Weinstock <jakobweinstock@gmail.com>
2021-02-24 08:25:28 -0700

    call tink-reboot directly, as its the abstraction
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,5 @@
 cp -a /tink-reboot /host/usr/sbin/tink-reboot
 chroot /host <<EOT
 /usr/sbin/tink-reboot -install
-/usr/sbin/tink-reboot -start
-/usr/sbin/tink-reboot -run
+/usr/sbin/tink-reboot -start || /usr/sbin/tink-reboot -daemon
 EOT
